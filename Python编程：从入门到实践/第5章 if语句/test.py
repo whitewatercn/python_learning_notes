878f3c652cda94849c31c73844601ad9559957e7
whtiewatercn <58654928+whitewatercn@users.noreply.github.com>
2020-10-25 22:32:36 +0800

    turtle库

--- Python编程：从入门到实践/第5章 if语句/test.py
+++ Python编程：从入门到实践/第5章 if语句/test.py
@@ -1,7 +1,7 @@
 #5.2.6
-#colors = ['red','whtie','black']
-#if 'yellow' in colors:
+colors = ['red','whtie','black']
+if 'yellow' in colors:
     #print ('bingo')
-#else: 
-    #print('sorry')
-#'red' in colors 
+else: 
+    #print('sorry')#
+ 'red' in colors 
